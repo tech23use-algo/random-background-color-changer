d1126f051788f1877581944fd2a60c7645f183ba
tech23use-algo <tech23use@gmail.com>
2026-07-02 19:45:18 +0530

    Initial commit. Random background color generator.

---
+++ README.md
@@ -0,0 +1,20 @@
+## Ranadom Background Color Changer.
+
+- A simple JavaScript project that generates random background colors at regular intervals and stops the color change when button is clicked.
+
+# Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+# What I learned 
+
+- Usage of setInterval, clearInterval.
+- About setTimeout and clearTimeout which is used for different use case.
+- Generation of random colors using hex values and Math.random() and Math.floor() functions.
+- Managing interval IDs to prevent the running of multiple intervals at the same time.
+
+# Note
+
+The project was being coded in the tutorial, and I coded it side by side. It also helped me understand how can the codes be more optimized.
---
+++ script.js
@@ -0,0 +1,31 @@
+const randomcolor = function() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color
+}
+
+let intervalId;
+const startChangingColor = function() {
+    if(intervalId == null) {
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor= randomcolor();
+}
+}
+
+const stopChangingColor = function() {
+        clearInterval(intervalId);
+        intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
+
+
+
